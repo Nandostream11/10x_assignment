b8b6bb57b0338fd049dd53673165c6c2890d2904
Anand Vardhan <anandvk113@gmail.com>
2025-11-01 07:25:35 +0530

    navigation outer loop for global planning

--- README.md
+++ README.md
@@ -1,23 +1,61 @@
 # Custom DWA Local Planner in ROS2 Humble with Turtlebot3
 
 Steps to run the assignment:-
+Installations:
+- Ensure ROS2 Humble is installed and setup
+- Gazebo Classic is installed
+- Set waffle as defauft in export
+    ```bash
+    sudo apt install -y ros-humble-turtlebot3* 
+    export TURTLEBOT3_MODEL=waffle
+    ```
 1. Setup the workspace
-```
-mkdir 10x_av_ws
-cd 10x_av_ws
-git clone https://github.com/Nandostream11/10x_assignment.git ./src
-```
-2. Build the workspace
-```
-cd ~/10x_av_ws
-colcon build
-```
+    ```bash
+    mkdir 10x_av_ws
+    cd 10x_av_ws
+    git clone https://github.com/Nandostream11/10x_assignment.git ./src
+    ```
+2. Install dependencies and Build the workspace
+    ```bash
+    cd ~/10x_av_ws
+    rosdep install --from-paths src --ignore-src -r -y
+    colcon build
+    ```
 3. Launch Turtlebot3 in a world
+    ```bash
+    source ~/10x_av_ws/install/setup.bash
+    ros2 launch turtlebot3_gazebo world.launch.py
+    ```
 4. Run the Custom DWA script
+    ```bash
+    source ~/10x_av_ws/install/setup.bash
+    ros2 run dwa_custom_planner nav_goal_dwa
+    ```
+5. Call the service to pass goal coordinates[eg: (1.2,2.0)]
+    ```bash
+    source ~/10x_av_ws/install/setup.bash
+    ros2 service call /togoal dwa_custom-planner/srv/ToGoal "{goal_x: 1.2, goal_y: 2.0}"
+    ```
 
-If you only want to access the topic outputs instead of the simulation, run the below pre-recorded rosbag:-
-```
-cd ~/10x_av_ws
-ros2 bag play 10x_assg_bag 
-```
+File tree:-
+.
+├── dwa_custom_planner
+│   ├── CMakeLists.txt
+│   ├── include
+│   │   └── dwa_custom_planner
+│   │       └── dwa_planner_custom.hpp
+│   ├── package.xml
+│   ├── src
+│   │   └── nav_goal_dwa.cpp
+│   └── srv
+│       └── ToGoal.srv
+└── README.md
 
+The turtlebot3_gazebo package is also added to the src to simulate the Trutlebot3 
+
+Main algorithm Workflow-
+[nav_goal_dwa]: Receive goal(x,y) from service -> Send goal iteratively to dwa_planner_custom -> Publish sampled trajectories, best trajectory marker array(to dwa_trajectories) -> Publish the best velcoity pairs
+
+[dwa_planner_custom]: 
+
+Below is the video demonstration:
---
+++ dwa_custom_planner/src/nav_goal_dwa.cpp
@@ -0,0 +1,123 @@
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/point.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "visualization_msgs/msg/marker_array.hpp"
+#include "dwa_custom_planner/srv/to_goal.hpp"
+#include "dwa_custom_planner/dwa_planner_custom.hpp"
+
+using namespace std::placeholders;
+
+class NAVGOALDWA : public rclcpp::Node
+{
+ public:
+    NAVGOALDWA() : Node("nav_goal_dwa")
+    {
+        service_ = this->create_service<dwa_custom_planner::srv::ToGoal>("togoal", std::bind(&NAVGOALDWA::goal_callback, this, _1, _2));
+
+        marker_pub_ = this->create_publisher<visualization_msgs::msg::MarkerArray>("dwa_trajectories", 10);
+        cmd_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+
+        RCLCPP_INFO(this->get_logger(), "DWA Service Node started. Waiting for goal...");
+    }
+ private:
+    rclcpp::Service<dwa_custom_planner::srv::ToGoal>::SharedPtr service_;
+    rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr marker_pub_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_pub_;
+
+    void goal_callback(const std::shared_ptr<dwa_custom_planner::srv::ToGoal::Request> request, 
+        std::shared_ptr<dwa_custom_planner::srv::ToGoal::Response> response)
+    {
+        RCLCPP_INFO(this->get_logger(), "Received goal: (%.2f, %.2f)", request->goal_x, request->goal_y);
+        auto dwa = std::make_shared<DWAPlannerCustom>();
+        dwa->setGoal(request->goal_x, request->goal_y);
+        rclcpp::Rate rate(10.0);       // 10 Hz
+
+        double dist_to_goal = std::numeric_limits<double>::max();
+
+        while (rclcpp::ok() && dist_to_goal > 0.15 )
+        {
+            // Compute best velocity command and trajectories
+            auto [best_v, best_w, all_trajs] = dwa->computeBestTrajectory();
+
+            // Extract best trajectory (first)
+            std::vector<geometry_msgs::msg::Point> best_traj;
+            if (!all_trajs.empty())
+                best_traj = all_trajs.front();
+
+            // Compute distance to goal from the last point
+            if (!best_traj.empty()) {
+                auto last = best_traj.back();
+                dist_to_goal = std::hypot(request->goal_x - last.x, request->goal_y - last.y);
+            }
+                    // Publish visualization and command
+            publishTrajectories(all_trajs, best_traj);
+            publishCmdVel(best_v, best_w);
+            RCLCPP_INFO(this->get_logger(),"v=%.2f w=%.2f dist=%.2f", best_v, best_w, dist_to_goal);
+
+            rclcpp::spin_some(dwa);  // let subscriptions update odom/scan
+            rate.sleep();
+        }
+        // Stop the robot
+        publishCmdVel(0.0, 0.0);
+        RCLCPP_INFO(this->get_logger(), "Goal reached!");
+        // Goal response 
+        response->linear_vel = 0.0;
+        response->angular_vel = 0.0;
+        response->traj_points.clear();
+    }
+
+    void publishTrajectories(const std::vector<std::vector<geometry_msgs::msg::Point>> &all_trajs, 
+        const std::vector<geometry_msgs::msg::Point> &best_traj)
+    {
+        visualization_msgs::msg::MarkerArray arr;
+        int id = 0;
+        for (const auto &traj : all_trajs)
+        {
+            auto m = visualization_msgs::msg::Marker();
+            m.header.frame_id = "odom";
+            m.header.stamp = this->now();
+            m.ns = "dwa_trajs";
+            m.id = id++;
+            m.type = m.LINE_STRIP;
+            m.action = m.ADD;
+            m.scale.x = 0.02;
+            m.color.r = m.color.g = m.color.b = 0.5;
+            m.color.a = 0.4;
+            m.points = traj;
+            arr.markers.push_back(m);
+        }
+        if (!best_traj.empty())
+        {
+            auto m = visualization_msgs::msg::Marker();
+            m.header.frame_id = "odom";
+            m.header.stamp = this->now();
+            m.ns = "dwa_trajs";
+            m.id = id++;
+            m.type = m.LINE_STRIP;
+            m.action = m.ADD;
+            m.scale.x = 0.04;
+            m.color.g = 1.0;
+            m.color.a = 1.0;
+            m.points = best_traj;
+            arr.markers.push_back(m);
+        }
+        marker_pub_->publish(arr);
+    }
+
+
+    void publishCmdVel(double v, double w)
+    {
+        geometry_msgs::msg::Twist cmd;
+        cmd.linear.x = v;
+        cmd.angular.z = w;
+        cmd_pub_->publish(cmd);
+    }
+};
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<NAVGOALDWA>());
+    rclcpp::shutdown();
+    return 0;
+}
